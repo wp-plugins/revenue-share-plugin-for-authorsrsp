7605be8a390815dcbf236862b718e807acb4af6a
obscurant1st <obscurant1st@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-04 14:12:58 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/revenue-share-plugin-for-authorsrsp/trunk@1059527 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors:
 Donate link: http://obscurant1st.biz/blog/donate-support/
 Tags: Adense, Free Revenue Share, Guest Bloggers, plugin, Revenue Share, Wordpress, Wordpress plugin, Wordpress Revenue Share
 Requires at least: 3.0.1
-Tested up to: 3.5.1
-Stable tag: 1.0.3
+Tested up to: 4.1
+Stable tag: 2.0.1
 License: GPLV2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -13,9 +13,9 @@ The plugin enables adsense revenue sharing for authors on your wordpress site.
 == Description ==
 Revenue Share Wordpress Plugin for Authors:
 
-This plugin helps you to give a small reward to your guest blog writers in terms of revenue. Their own advertisements will be displayed for all their posts on your blog for 50% of the impressions. For this they just have to fill up a field in their profile section with their adsense publisher id.
+This plugin helps you to give a small reward to your guest blog writers in terms of revenue. Their own advertisements will be displayed for all their posts on your blog. The for 50% of the impressions. For this they just have to fill up a field in their profile section with their adsense publisher id.
 
-This plugin adds a field to all your blog post authors' profiles for inserting their Google adsense publisher id and creates a Settings page which will be accessible to admin of the website. The settings page include a field to fill up admin's adsense pub id as well as the position where the ad should be displayed. For now this plugin supports 2 adspots, Top of the post, Middle, and Bottom.
+This plugin adds a field to all your blog post authors' profiles for inserting their Google adsense publisher id and creates a Settings page which will be accessible to admin of the website. The settings page include a field to fill up admin's adsense pub id as well as the position where the ad should be displayed along with the option to add a widget on the sidebar.
 
 Features:<br />
 1. Revenue for Both Admins and Authors<br />
@@ -23,6 +23,8 @@ Features:<br />
 3. No credit link on your blog<br />
 4. The simplest settings page you have ever seen for a plugin.<br />
 5. Supports 468x60px ads(easy customization).<br />
+6. *New* Now supports widget ads
+7. *New* Can change the ad percentage like you guys requested.
 
 Note: Please use it wisely and never add adsense ads on websites with illegal contents or website which are not in terms with Google Adsense TOS.
 Also be careful about the number of ads since adsense doesn't allow websites to have more than 3 ads on any webpage.
@@ -34,21 +36,26 @@ Upload the contents of the zip file to your plugin directory.
 
 == Frequently Asked Questions ==
 = Where do I contact you in case I have any doubts regarding the usage of your plugin? =
-You can contact me via my personal website http://www.obscurant1st.biz
+You can contact me via my personal website http://www.techtuft.com
 
 == Screenshots ==
 1. **Admin Settings** - Screenshot for the Admin Menu
-2. **Admin Settings - Complete** - Dropdown Box for the Adspot
-3. **User Profile Field** - Publisher ID field in the User profile
+2. **User Profile Field** - Publisher ID field in the User profile
+3. **Sidebar Ad Widget** - Create and configure a widget to display ad with the revenue sharing capability.
 
 == Changelog ==
 = 1.0.2 =
 * Fixed a Stylesheet error
 = 1.0.3 =
 * Fixed a few bugs
+= 2.0.1 =
+* Added option to set the ad share percentage
+* Added a sidebar widget to display the ad with the revenue sharing capability
 
 == Upgrade Notice ==
 = 1.0.2 =
 * Update the plugin to get a better admin page for RSP.
 = 1.0.3 =
 * Bug Fix Release
+= 2.0.1 =
+* All New Revamped RSP
---
+++ widgetad.php
@@ -0,0 +1,127 @@
+<?php // Creating the widget
+require_once(plugin_dir_path( __FILE__ ) . '/rShareAuthors.php');
+class rspwidget extends WP_Widget {
+
+    function __construct() {
+    parent::__construct(
+    // Base ID of your widget
+    'rspwidget',
+
+    // Widget name will appear in UI
+    __('RSP Ad Widget', 'rspwidget_ad'),
+
+    // Widget description
+    array( 'description' => __( 'Revenue share Plugin(RSP) Widget', 'rspwidget_ad' ), )
+        );
+    }
+
+    // Creating widget front-end
+    // This is where the action happens
+    public function widget( $args, $instance ) {
+        echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+        // before and after widget arguments are defined by themes
+        // This is where you run the code and display the output
+        $addimensions = split ("\x", $instance['widgetads']);
+        echo adsensewidgetad($addimensions[0], $addimensions[1]);
+        echo $args['after_widget'];
+        global $post;
+        $authorId = $post->ID;
+    }
+
+    // Widget Backend
+    public function form( $instance ) {
+        $title = ! empty( $instance['title'] ) ? $instance['title'] : __( '', 'rspwidget_ad' );
+        $widgetads = ! empty( $instance['widgetads'] ) ? $instance['widgetads'] : __( '', 'rspwidget_ad' );
+    // Widget admin form
+?>
+    <p>
+        <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'title' ); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+      </p><p>
+        <label for="<?php echo $this->get_field_id( 'widgetads' ); ?>"><?php _e( 'Ad dimensions:', 'widgetads' ); ?></label>
+        <select id="<?php echo $this->get_field_id( 'widgetads' ); ?>" name="<?php echo $this->get_field_name( 'widgetads' ); ?>" type="text">
+            <option value="300x250" <?php echo "300x250" == $widgetads ? "selected" : ""; ?>>300x250px</option>
+            <option value="336x280" <?php echo "336x280" == $widgetads ? "selected" : ""; ?>>336x280px</option>
+            <option value="300x600" <?php echo "300x600" == $widgetads ? "selected" : ""; ?>>300x600px</option>
+            <option value="320x100" <?php echo "320x100" == $widgetads ? "selected" : ""; ?>>320x100px</option>
+        </select>
+    </p>
+<?php
+    }
+
+    // Updating widget replacing old instances with new
+    public function update( $new_instance, $old_instance ) {
+            $instance = array();
+            //$instance = $old_instance;
+            $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+            $instance['widgetads'] = ( ! empty( $new_instance['widgetads'] ) ) ? strip_tags( $new_instance['widgetads'] ) : '';
+            return $instance;
+    }
+} // Class rspwidget ends here
+
+//create the ad for the widget
+function adsensewidgetad($x, $y) {
+            $options = get_option('RSP_options');
+            if (!get_option('RSP_options')) {
+                return 'Configure RSP settings!';
+            }
+            $position = '';
+            if (array_key_exists('radio_option1', $options)) {
+                $position =  $options['radio_option1'];
+            }
+            /*if (array_key_exists('radio_option2', $options)) {
+            $position =  $options['radio_option2'];
+            }
+            if (array_key_exists('radio_option3', $options)) {
+            $position =  $options['radio_option3'];
+            }*/
+            //get the admin pecentage
+            global $post;
+            $authorId = $post->ID;
+
+            $adpercent = $options['adshare_percentage'];
+            //store both adsense pub ids(author and admin)
+            if(get_the_author_meta( 'RSP_text_string', $authorId ) != ""){
+                $input1 = $options['RSP_text_string'];
+                $input2 = get_the_author_meta( 'RSP_text_string', $authorId );
+            } else {
+                $input1 = $options['RSP_text_string'];
+                $input2 = $input1;
+            }
+            //randomize the admin/author accordingly
+            if (rand(1,100) > $adpercent) $flag = $input1; else $flag = $input2;
+
+            if ($input == 'pub-0000') {
+                return 'Configure PUB ID!';
+            }
+        $ad_content = '<div align=center style="background-color:black;"><script type="text/javascript"><!--
+google_ad_client = "ca-'.$flag.'";
+google_ad_width = '.$x.';
+google_ad_height = '.$y.';
+//-->
+</script>
+<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script></div>';
+        if($position == 'Top') {
+            return $ad_content.'<br />'.$content;
+        }
+        if($position == 'Bottom') {
+            return $content.'<br />'.$ad_content;
+        }
+        if($position == 'Middle') {
+            $count_words = strlen($content)/2;
+            $insert_ad = strpos($content, '. ', $count_words);
+            $ad_content = '<br />'.$ad_content.'<br />';
+            return substr_replace($content, $ad_content, $insert_ad+2, 0);
+        }
+            return $ad_content;
+    }
+
+// Register and load the widget
+function rsp_load_widget() {
+	register_widget( 'rspwidget' );
+}
+add_action( 'widgets_init', 'rsp_load_widget' );
+?>
